c3498f16b8047c3c5b94429795dba33bea5e726d
Ben Vaughn <bevon@protonmail.com>
2020-06-07 22:03:48 -0600

    Refactored the images functions in scoreboard.

--- scoreboard.py
+++ scoreboard.py
@@ -19,10 +19,7 @@ class Scoreboard:
 		self.font = pygame.font.SysFont(None, 48)
 
 		# Prepare the initial score images.
-		self.prep_score()
-		self.prep_high_score()
-		self.prep_level()
-		self.prep_ships()
+		self.prep_images()
 
 	def prep_score(self):
 		"""Turn the score into a rendered image."""
@@ -80,5 +77,12 @@ class Scoreboard:
 			ship.rect.x = 10 + ship_number * ship.rect.width
 			ship.rect.y = 10
 			self.ships.add(ship)
+
+	def prep_images(self):
+		"""Prepare all the score/level/ship readouts."""
+		self.prep_score()
+		self.prep_high_score()
+		self.prep_level()
+		self.prep_ships()
 
 	
